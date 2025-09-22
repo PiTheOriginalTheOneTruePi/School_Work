b40f5eb9e99497f2002de2bbc79bc532369e28dd
Pi <filipe.pereira.sousa.pi@gmail.com>
2025-09-22 16:13:38 +0100

    flex1

--- est_dados/f1ex1.c
+++ est_dados/f1ex1.c
@@ -2,7 +2,23 @@
 
 int main(){
 	
-	printf("hello world!");
-
+	int n,val,soma1;
+	float media;
+	printf("Numero de elementos: ");
+	scanf("%d",&n);
+	float vac[n];
+	soma1=0;
+	printf("\nfor loops: \n");
+	for(int i=0;i<n;i++){
+		
+		printf("\ninserir %d valor: ", i+1);
+		scanf("%d",&val);
+		soma1=soma1+val;
+		printf("%d\n",soma1);
+	}
+	media = (float)soma1/n;
+	printf("\nTotal: %d", soma1);
+	printf("\nMedia: %f", media);
+	printf("\nMedia: %.2f",media);
 return 0;
 }
